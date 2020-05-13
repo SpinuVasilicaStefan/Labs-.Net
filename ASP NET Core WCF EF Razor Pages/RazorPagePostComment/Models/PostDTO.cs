c3e7b1f55e2356b947bd33640e17c497e6d65a8d
Spinu Vasilica-Stefan <61910783+SpinuVasilicaStefan@users.noreply.github.com>
2020-05-13 13:51:31 +0300

    Add files via upload

---
+++ ASP NET Core WCF EF Razor Pages/RazorPagePostComment/Models/PostDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace RazorPagePostComment.Models
+{
+    [DataContract]
+    public partial class PostDTO
+    {
+        public PostDTO()
+        {
+            this.Comments = new List<CommentDTO>(); //new HashSet<Comment>();
+        }
+        [DataMember]
+        public int PostId { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+
+        [DataMember]
+        public string Domain { get; set; }
+
+        [DataMember]
+        public System.DateTime Date { get; set; }
+        [DataMember]
+        public string Title { get; set; }
+        [DataMember]
+        public virtual List<CommentDTO> Comments { get; set; }
+    }
+}
